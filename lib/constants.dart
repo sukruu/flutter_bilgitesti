9a62585eed992ad12eb224c1e2a827043ef91188
sukruu <31570515+sukruu@users.noreply.github.com>
2022-11-03 17:19:15 +0300

    ilk versiyon

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kDogruIconu = Icon(Icons.mood, color: Colors.green);
+const kYanlisIconu = Icon(Icons.mood_bad, color: Colors.red);
---
+++ lib/soru.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Soru {
+  late String soruMetni;
+  late bool soruYaniti;
+
+  Soru({required this.soruMetni, required this.soruYaniti});
+}
